0b5295f70a22fd81d421824678fadf1d4a77fa7c
Ewan <chl8263@naver.com>
2020-02-19 20:50:38 -0800

    spring-boot/Actuator

---
+++ Springboot/src/main/java/me/ewan/ewan/Actuator/ActuatorController.java
@@ -0,0 +1,13 @@
+package me.ewan.ewan.Actuator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActuatorController {
+
+    @GetMapping("/actuator")
+    public String actuator(){
+        return "actuator";
+    }
+}
