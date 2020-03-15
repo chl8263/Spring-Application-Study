4bd3670d4d020738eb2532949c48c83e1e3e2be0
Ewan <chl8263@naver.com>
2020-03-14 23:35:10 -0700

    5. Cascade

---
+++ SpringData/src/main/java/me/ewan/springdata/domain/Comment.java
@@ -0,0 +1,32 @@
+package me.ewan.springdata.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Post post;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
