4467bcd4ff89909561321584dce9ddb3841bae47
Ewan <chl8263@naver.com>
2020-03-16 23:48:40 -0700

    JPA common

--- SpringData/src/main/java/me/ewan/springdata/JpaRunner.java
+++ SpringData/src/main/java/me/ewan/springdata/JpaRunner.java
@@ -20,7 +20,7 @@ import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
-//@Component
+@Component
 public class JpaRunner implements ApplicationRunner {
 
     @PersistenceContext
--- SpringData/src/main/java/me/ewan/springdata/repositrory/PostRepository2.java
+++ SpringData/src/main/java/me/ewan/springdata/repositrory/PostRepository2.java
@@ -1,9 +1,14 @@
 package me.ewan.springdata.repositrory;
 
 import me.ewan.springdata.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface PostRepository2 extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
