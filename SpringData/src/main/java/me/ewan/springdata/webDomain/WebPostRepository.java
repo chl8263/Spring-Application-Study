7a7e9c3fa6c613097f5c75a5c90dc8302a78666d
Ewan <chl8263@naver.com>
2020-03-22 20:24:49 -0700

    Spring data save

--- SpringData/src/main/java/me/ewan/springdata/webDomain/WebPostRepository.java
+++ SpringData/src/main/java/me/ewan/springdata/webDomain/WebPostRepository.java
@@ -1,6 +1,26 @@
 package me.ewan.springdata.webDomain;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.NamedNativeQuery;
+import java.util.List;
 
 public interface WebPostRepository extends JpaRepository<WebPost, Long> {
+
+    List<WebPost> findByTitleStartingWith(String title);
+
+    //@Query(value = "SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
+    List<WebPost> findByTitle(@Param("Title") String title);
+
+    //@Query(value = "SELECT p FROM Post WHERE id = 1", nativeQuery = true)
+    @Query(value = "SELECT p FROM WebPost AS p WHERE p.id = ?1")
+    List<WebPost> findById(String title, Sort sort);
+
+    @Modifying  //For update
+    @Query(value = "UPDATE WebPost p SET p.title = ?1 WHERE p.id = ?2")
+    int updateTitle(String jpa, Long id);
 }
---
+++ SpringData/src/test/java/me/ewan/springdata/JPAPostTest.java
@@ -0,0 +1,110 @@
+package me.ewan.springdata;
+
+import me.ewan.springdata.webDomain.WebPost;
+import me.ewan.springdata.webDomain.WebPostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class JPAPostTest {
+
+    @Autowired
+    private WebPostRepository webPostRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void updateTitle(){
+        WebPost webPost = new WebPost();
+        //webPost.setId(1L);
+        webPost.setTitle("jpa");
+        webPost.setTitle("jpa2");
+        //WebPost savedWebPost = webPostRepository.save(webPost);
+
+//        int update = webPostRepository.updateTitle("jpa2", webPost.getId());
+//        assertThat(update).isEqualTo(1);
+
+        List<WebPost> saveWebPost = webPostRepository.findAll();
+        assertThat(saveWebPost.get(0).getTitle()).isEqualTo("jpa2");
+    }
+
+    @Test
+    public void findById(){
+
+        WebPost webPost = new WebPost();
+        //webPost.setId(1L);
+        webPost.setTitle("jpa");
+        WebPost savedWebPost = webPostRepository.save(webPost);
+
+        savedWebPost.setTitle("Spring");
+        webPostRepository.save(savedWebPost);
+
+        List<WebPost> all = webPostRepository.findById("Spring", Sort.by("Title"));
+        assertThat(all.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void findByTitle(){
+
+        WebPost webPost = new WebPost();
+        //webPost.setId(1L);
+        webPost.setTitle("jpa");
+        WebPost savedWebPost = webPostRepository.save(webPost);
+
+        savedWebPost.setTitle("Spring");
+        webPostRepository.save(savedWebPost);
+
+        List<WebPost> all = webPostRepository.findByTitle("Spring");
+        assertThat(all.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void findByTitleStartingWith(){
+
+        WebPost webPost = new WebPost();
+        //webPost.setId(1L);
+        webPost.setTitle("jpa");
+        WebPost savedWebPost = webPostRepository.save(webPost);
+
+        savedWebPost.setTitle("SpringjpaTest");
+        webPostRepository.save(savedWebPost);
+
+        List<WebPost> all = webPostRepository.findByTitleStartingWith("Spring");
+        assertThat(all.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void save(){
+
+        WebPost webPost = new WebPost();
+        //webPost.setId(1L);
+        webPost.setTitle("jpa");
+        WebPost savedWebPost = webPostRepository.save(webPost);
+
+        savedWebPost.setTitle("jpaTest");
+        webPostRepository.save(savedWebPost);
+
+//        WebPost webPost2 = new WebPost();
+//        webPost2.setId(1L);
+//        webPost2.setTitle("jpa2");
+//        webPostRepository.save(webPost2);
+
+        List<WebPost> all = webPostRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+    }
+}
