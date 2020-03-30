4f07be69a568e184f345c9806774ecf292f53bd7
Ewan <chl8263@naver.com>
2020-03-29 22:18:11 -0700

    Spring MVC Interceptor implement

--- SpringMVCPrinciple/src/main/java/com/example/springmvcprinciple/config/WebConfig.java
+++ SpringMVCPrinciple/src/main/java/com/example/springmvcprinciple/config/WebConfig.java
@@ -1,14 +1,18 @@
 package com.example.springmvcprinciple.config;
 
+import com.example.springmvcprinciple.interceptor.AnotherInterceptor;
+import com.example.springmvcprinciple.interceptor.GreetingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class WebConfig{//} implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addFormatters(FormatterRegistry registry) {
-//        registry.addFormatter(new PersonFormatter());
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor()).order(-1);
+    }
 }
--- SpringMVCPrinciple/src/main/java/com/example/springmvcprinciple/interceptor/AnotherInterceptor.java
+++ SpringMVCPrinciple/src/main/java/com/example/springmvcprinciple/interceptor/AnotherInterceptor.java
@@ -6,20 +6,20 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class GreetingInterceptor implements HandlerInterceptor {
+public class AnotherInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("preHandle 1");
+        System.out.println("preHandle 2");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("postHandle 1");
+        System.out.println("postHandle 2");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("afterComplete 1");
+        System.out.println("afterComplete 2");
     }
 }
