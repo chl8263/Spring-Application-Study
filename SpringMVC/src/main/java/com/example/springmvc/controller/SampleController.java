d041aa97805ff7faa7337eeb1feb1cc81bbea5c6
Ewan <chl8263@naver.com>
2020-03-30 21:51:16 -0700

    SpringMVC URL mapping

---
+++ SpringMVC/src/main/java/com/example/springmvc/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.example.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+
+    //@RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.PUT})
+    //@GetMapping
+    @RequestMapping({"/hello" , "hi"})
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
