c380be6d278ceb10bc63073cc616e2d06d8236ec
Ewan <chl8263@naver.com>
2020-04-05 22:49:39 -0700

    SpringMVC ResponseBody

--- SpringMVC/src/main/java/com/example/springmvc/controller/EventApi.java
+++ SpringMVC/src/main/java/com/example/springmvc/controller/EventApi.java
@@ -3,6 +3,7 @@ package com.example.springmvc.controller;
 import com.example.springmvc.domain.Event;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,5 +30,22 @@ public class EventApi {
         }
 
         return event;
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> eventEvent2(@Validated Event event, BindingResult bindingResult){
+        //public Event eventEvent(HttpEntity<Event> request){
+        // save event
+
+        //MediaType mediaType = request.getHeaders().getContentType();
+        //System.out.println(mediaType);
+
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(x -> {
+                System.out.println(x);
+            });
+        }
+
+        return ResponseEntity.ok(event);
     }
 }
