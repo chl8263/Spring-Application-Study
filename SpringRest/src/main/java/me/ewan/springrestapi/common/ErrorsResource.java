87025089e24949d4fa3fcf67c2636cc2a8df33e0
Ewan <chl8263@naver.com>
2020-04-15 23:19:56 -0700

    Spring Rest - API index

---
+++ SpringRest/src/main/java/me/ewan/springrestapi/common/ErrorsResource.java
@@ -0,0 +1,8 @@
+package me.ewan.springrestapi.common;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.validation.Errors;
+
+public class ErrorsResource extends RepresentationModel {
+
+}
