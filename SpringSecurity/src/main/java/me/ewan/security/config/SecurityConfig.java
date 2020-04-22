694c2f5f98cc4c8cafe0488dbe1044a79cec9f68
Ewan <chl8263@naver.com>
2020-04-21 23:32:05 -0700

    Spring Security - Connect Jpa

--- SpringSecurity/src/main/java/me/ewan/security/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/me/ewan/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package me.ewan.security.config;
 
+import me.ewan.security.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "info").permitAll()
+                .mvcMatchers("/", "info", "account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
@@ -24,10 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("ewan").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}!@#").roles("ADMIN");
-    }
+    // in memory
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("ewan").password("{noop}123").roles("USER").and()
+//                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+//    }
 }
